d0a0a6b364832078b265b4fe8c06f3403fbb1e22
ldingPU <146761717+ldingPU@users.noreply.github.com>
2024-01-23 16:02:25 -0500

    Update ssmain.py

--- ssmain.py
+++ ssmain.py
@@ -112,12 +112,12 @@ if input_number:
             df1 = pd.DataFrame(pmatrixabs, columns=modeNames)
             df1.insert(0, "statevariables", stateVariableNames, True)           
             df1.loc[df1[modeNames[number-1]] < 0.02, 'statevariables'] = 'Other states'  # Represent state variables with a relatively larger participation factor
-            figpie1 = px.pie(df1, values=modeNames[number-1], names='statevariables', title='Participation factor analysis of mode '+str(number))
+            figpie1 = px.pie(df1, values=modeNames[number-1], names='statevariables', title='Participation Factor Analysis of Mode '+str(number))
             figpie1.update_layout(title={'text':'Participation factor analysis of mode '+str(number),'x':0.415,'xanchor':'center'})
             df2 = pd.DataFrame(pmatrixabs, columns=modeNames)
             df2.insert(0, "statevariables", stateVariableNames, True)           
             df2.loc[df2[modeNames[number]] < 0.02, 'statevariables'] = 'Other states'  # Represent state variables with a relatively larger participation factor
-            figpie2 = px.pie(df2, values=modeNames[number], names='statevariables', title='Participation factor analysis of mode '+str(number+1))
+            figpie2 = px.pie(df2, values=modeNames[number], names='statevariables', title='Participation Factor Analysis of Mode '+str(number+1))
 
             # Update font size, color and style
             figpie1.update_layout(title={'text':'Participation factor analysis of mode '+str(number),'x':0.415,'xanchor':'center','font': {'size': 18, 'color': 'black','family': 'Arial'}}) # Update the layout for the title
@@ -157,7 +157,7 @@ frequency = eigvals.imag/2/math.pi
 dampingratio = -eigvals.real/np.sqrt(realpart*realpart+imagpart*imagpart)
 list_of_tuples = list(zip(mode, realpart, imagpart, abs(frequency), dampingratio))
 df = pd.DataFrame(list_of_tuples,
-                  columns = ["mode", "real", "imag", "freq(Hz)", "damping ratio"])
+                  columns = ["mode", "real", "imag", "freq (Hz)", "damping ratio"])
 
 # Convert DataFrame to HTML and style it
 html = df.to_html(index=False)
